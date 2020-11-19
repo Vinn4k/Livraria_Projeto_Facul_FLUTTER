f862f4d25dee2155dc2e91516bb2ee0b851bbb5c
Emison <emisondias1@gmail.com>
2020-11-19 12:06:29 -0300

    layout Login Concluido

--- lib/views/login_Viewer.dart
+++ lib/views/login_Viewer.dart
@@ -13,73 +13,136 @@ class _LoginVState extends State<LoginV> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.brown,
       body: Container(
 
-        child:Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.08,
-            ),
-             Center(child: Text("Vinns Books",style:
-             TextStyle(
-               color: Color(cor),
-                 fontFamily:'font1',
-                 fontSize: MediaQuery.of(context).size.height*0.05,
-
-             ),
-             )
-             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.3,
-            ),
-            Container(
-              child: Text("Para Continuar com o login Clique no G",style:
-              TextStyle(
-                color: Color(cor2),
-                fontSize: MediaQuery.of(context).size.height*0.02,
-
+        child:SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.19,
               ),
-              )
-            ),
+               Center(child: Text("Vinns Books",style:
+               TextStyle(
+                 color: Colors.amber,
+                   fontFamily:'Ral',
+                   fontSize: MediaQuery.of(context).size.width*0.19,
 
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.01,
-            ),
+               ),
+               )
 
+               ),
 
-            Center(
-              child: Container(
-                  height: MediaQuery.of(context).size.height*0.3,
-                  width: MediaQuery.of(context).size.width*0.3,
-                  alignment: Alignment.center,
+              Container(
 
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
+                child: Text("Seus livros favoritos em um Clique ",style:
+                TextStyle(
+                  color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.width*0.03,
 
-                  ),
-                  child:FlatButton
-                    ( child:
-                  Text("G", style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.height*0.1,
-                  )
-                  ),
-                    onPressed:() async{
-
-
-
-                      }
-
-
-                  )
+                ),
+                )
               ),
 
-            )
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.3,
+              ),
 
-          ],
+
+              Container(
+                color: Color(cor),
+                  width: MediaQuery.of(context).size.width*0.9,
+                 height: MediaQuery.of(context).size.height*0.10,
+                child: FlatButton(
+                  onPressed: (){},
+                  child: Row(
+                    children: [
+
+                      Container(
+                        child:  Text("Login Com Google",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width*0.05,
+                  )
+                          ),
+
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width*0.20,
+
+                      ),
+                      Container(
+                          height: MediaQuery.of(context).size.height*0.18,
+                          width: MediaQuery.of(context).size.width*0.18,
+                          alignment: Alignment.center,
+
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+
+                          ),
+                          child:
+                          Text("G", style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.height*0.08,
+                          )
+                          ),
+
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.02,
+              ),
+              Container(
+                color: Color(cor),
+                width: MediaQuery.of(context).size.width*0.9,
+                height: MediaQuery.of(context).size.height*0.10,
+                child: FlatButton(
+                  onPressed: (){},
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width*0.02,
+
+                      ),
+                      Container(
+                        child: Text("Sair", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.width*0.1,
+                        )
+                        ),
+                      ),
+
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width*0.42,
+
+                      ),
+
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.redAccent,
+                        size: MediaQuery.of(context).size.height*0.08,
+                      ),
+                    ],
+                  ),
+                ),
+
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.01,
+
+              ),
+              Container(child: Text("Todos Direitos Reservado",style: TextStyle(
+                color: Colors.white
+              ),),)
+
+            ],
+          ),
         ) ,
       ),
     );
