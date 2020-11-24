dfd8e0054200a71c6d759b2fd00a9abfce4d7f06
Emison <emisondias1@gmail.com>
2020-11-24 19:36:40 -0300

    Login com google implementado e funcionando 24/11

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:vinns_books/views/home_View.dart';
 import 'package:vinns_books/views/login_Viewer.dart';
 void main() async {
 
@@ -10,9 +8,4 @@ void main() async {
     debugShowCheckedModeBanner: false,
 
   ));
-QuerySnapshot snapshot=await Firestore.instance.collection("Teste").getDocuments();
-snapshot.documents.forEach((d) {print(d.data); });
-
-
-
 }
--- lib/views/login_Viewer.dart
+++ lib/views/login_Viewer.dart
@@ -1,47 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:vinns_books/Controllers/firebaseController.dart';
 
+import 'home_View.dart';
 
 class LoginV extends StatefulWidget {
   @override
   _LoginVState createState() => _LoginVState();
 }
+
 class _LoginVState extends State<LoginV> {
-  final  cor=0xff5c2441;
-  final cor2=0xffb34866;
-  final  cor3=0xff4b7c9;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  FirebaseUser currentUser;
+
+  final cor = 0xff5c2441;
+  final cor2 = 0xffb34866;
+  final cor3 = 0xff4b7c9;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      currentUser = user;
+
+      if (currentUser != null) {
+        Navigator.pop(context);
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return homeViewer();
+        }));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown,
       body: Container(
-
-        child:SingleChildScrollView(
+        child: SingleChildScrollView(
           child: Column(
             children: [
               SizedBox(
                 height: MediaQuery.of(context).size.height*0.19,
               ),
-               Center(child: Text("Vinns Books",style:
-               TextStyle(
-                 color: Colors.amber,
-                   fontFamily:'Ral',
-                   fontSize: MediaQuery.of(context).size.width*0.19,
+              Center(child: Text("Vinns Books",style:
+              TextStyle(
+                color: Colors.amber,
+                fontFamily:'Ral',
+                fontSize: MediaQuery.of(context).size.width*0.19,
 
-               ),
-               )
+              ),
+              )
 
-               ),
+              ),
 
               Container(
 
-                child: Text("Seus livros favoritos em um Clique ",style:
-                TextStyle(
-                  color: Colors.white,
-                  fontSize: MediaQuery.of(context).size.width*0.03,
+                  child: Text("Seus livros favoritos em um Clique ",style:
+                  TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width*0.03,
 
-                ),
-                )
+                  ),
+                  )
               ),
 
               SizedBox(
@@ -51,20 +74,38 @@ class _LoginVState extends State<LoginV> {
 
               Container(
                 color: Color(cor),
-                  width: MediaQuery.of(context).size.width*0.9,
-                 height: MediaQuery.of(context).size.height*0.10,
+                width: MediaQuery.of(context).size.width*0.9,
+                height: MediaQuery.of(context).size.height*0.10,
                 child: FlatButton(
-                  onPressed: (){},
+                  onPressed: () async {
+                    FirebaseUser user = await _getuser();
+                    if (currentUser != null) {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) {
+                        return homeViewer();
+                      }
+                      ));
+                    }
+                    else {
+
+                    }
+                  },
+
+
                   child: Row(
                     children: [
 
                       Container(
-                        child:  Text("Login Com Google",style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.width*0.05,
-                  )
-                          ),
+                        child: Text("Login Com Google", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: MediaQuery
+                              .of(context)
+                              .size
+                              .width * 0.05,
+                        )
+                        ),
 
                       ),
                       SizedBox(
@@ -72,22 +113,22 @@ class _LoginVState extends State<LoginV> {
 
                       ),
                       Container(
-                          height: MediaQuery.of(context).size.height*0.18,
-                          width: MediaQuery.of(context).size.width*0.18,
-                          alignment: Alignment.center,
+                        height: MediaQuery.of(context).size.height*0.18,
+                        width: MediaQuery.of(context).size.width*0.18,
+                        alignment: Alignment.center,
 
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
 
-                          ),
-                          child:
-                          Text("G", style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            fontSize: MediaQuery.of(context).size.height*0.08,
-                          )
-                          ),
+                        ),
+                        child:
+                        Text("G", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.height*0.08,
+                        )
+                        ),
 
                       ),
                     ],
@@ -138,13 +179,37 @@ class _LoginVState extends State<LoginV> {
 
               ),
               Container(child: Text("Todos Direitos Reservado",style: TextStyle(
-                color: Colors.white
+                  color: Colors.white
               ),),)
 
             ],
           ),
-        ) ,
+        ),
       ),
     );
+  }
+}
+
+String name;
+
+Future<FirebaseUser> _getuser() async {
+  if (currentUser != null) return currentUser;
+  try {
+    final GoogleSignInAccount googleSignInAccount =
+    await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+    await googleSignInAccount.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken,
+    );
+    final AuthResult authResult =
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+    name = currentUser.displayName;
+
+    return user;
+  } catch (error) {
+    return null;
   }
 }
